647fe66f58ee22a30d7b67dafd6f0a370d3bbcf1
Yurka2048 <44337171+Yurka2048@users.noreply.github.com>
2022-02-01 19:38:41 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,47 @@
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+const save = document.getElementById('save')
+const clear = document.getElementById('clear')
+var img = new Image();
+let isDrawing = false
+
+canvas.width = window.innerWidth
+canvas.height = window.innerHeight - 50
+
+function getMousePos(canvas, evt) {
+    var rect = canvas.getBoundingClientRect();
+    return {
+      x: evt.clientX - rect.left,
+      y: evt.clientY - rect.top
+    };
+}
+
+canvas.addEventListener('mousedown', (e) => {
+    let pos = getMousePos(canvas, e)
+    isDrawing = true
+    ctx.beginPath();
+    ctx.strokeStyle = 'black';
+    ctx.moveTo(pos.x, pos.y);
+  })
+
+canvas.addEventListener('mouseup', () => {
+    isDrawing = false
+    ctx.closePath()
+})
+
+canvas.addEventListener('mousemove', (e) => {
+    if (isDrawing) {
+      let pos = getMousePos(canvas, e)
+      ctx.lineTo(pos.x, pos.y);
+  
+      ctx.stroke();
+    }
+})
+
+save.addEventListener('click', () => {
+    console.log(canvas.toDataURL())
+})
+
+clear.addEventListener('click', () => {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+})
