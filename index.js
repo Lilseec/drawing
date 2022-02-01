1a618dc257f91c5aa0317d79065657109acb0f6a
Yurka2048 <44337171+Yurka2048@users.noreply.github.com>
2022-02-01 20:58:45 +0200

    added smartphone support

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
+
 const save = document.getElementById('save')
 const clear = document.getElementById('clear')
 var img = new Image();
@@ -16,26 +17,56 @@ function getMousePos(canvas, evt) {
     };
 }
 
+function getFingerPos(e) {
+    return {
+        x: e.changedTouches[0].clientX,
+        y: e.changedTouches[0].clientY
+    }
+}
+
+
 canvas.addEventListener('mousedown', (e) => {
     let pos = getMousePos(canvas, e)
     isDrawing = true
     ctx.beginPath();
     ctx.strokeStyle = 'black';
+    ctx.lineWidth = 5
     ctx.moveTo(pos.x, pos.y);
   })
+
+canvas.addEventListener('touchstart', (e) => {
+    let pos = getFingerPos(e)
+    isDrawing = true
+    ctx.beginPath();
+    ctx.strokeStyle = 'black';
+    ctx.lineWidth = 5
+    ctx.moveTo(pos.x, pos.y);
+})
 
 canvas.addEventListener('mouseup', () => {
     isDrawing = false
     ctx.closePath()
 })
 
+canvas.addEventListener('touchend', () => {
+    isDrawing = false
+    ctx.closePath()  
+})
+
 canvas.addEventListener('mousemove', (e) => {
     if (isDrawing) {
       let pos = getMousePos(canvas, e)
       ctx.lineTo(pos.x, pos.y);
-  
       ctx.stroke();
     }
+})
+
+canvas.addEventListener('touchmove', (e) => {
+    if (isDrawing) {
+        let pos = getFingerPos(e)
+        ctx.lineTo(pos.x, pos.y);
+        ctx.stroke();
+      }
 })
 
 save.addEventListener('click', () => {
